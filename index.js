278c4b6b4b7ac8f2ff9959e402a53cc3d4fd79a2
Gary <galvarado1501@gmail.com>
2025-05-16 21:09:44 -0700

    project has been finalized and is ready for live server

--- index.js
+++ index.js
@@ -2,19 +2,61 @@ const apikey = import.meta.env.VITE_API_KEY;
 const searchInput = document.getElementById("search-bar")
 const searchBtn = document.getElementById("search-btn")
 const searchResults = document.getElementById("results")
+let watchList = JSON.parse(localStorage.getItem("watchlist"));
 
 searchBtn.addEventListener("click", getData);
 
 async function getData(){
     const res = await fetch(`https://www.omdbapi.com/?apikey=${apikey}&s=${searchInput.value}`);
     const data = await res.json();
-    console.log(data);
     for(let child of searchResults.children)
         child.style.display = "none";
         
     let moviesHtml = "";
-    for(let movie of data.Search){
-        moviesHtml += `<h3>${movie.Title}</h3>`;
+    let foundMovie = false;
+    if(data.Search){
+        for(let movie of data.Search){
+            let res2 = await fetch(`https://www.omdbapi.com/?apikey=${apikey}&i=${movie.imdbID}`)
+            let fullData = await res2.json();
+            if(fullData.Response === "True"){
+                foundMovie = true;
+                moviesHtml += `
+                    <div class="movies">
+                        <img class="movie-poster" src="${fullData.Poster}" alt="${fullData.Title} Poster">
+                        <div class="movie-wrapper">
+                            <div class="movie-title-rating">
+                                <h3 class="movie-title">${fullData.Title}</h3>
+                                <div class="rating">
+                                    <img src="/images/star.png" alt="Yellow Star">
+                                    <p>${fullData.imdbRating}</p>
+                                </div>
+                            </div>
+                            <div class="movie-runtime-genre">
+                                <p>${fullData.Runtime}</p>
+                                <p>${fullData.Genre}</p>
+                                <div class="btn-wrapper">
+                                    <button class="watchlist-btn" data-movie-id="${movie.imdbID}">+</button>
+                                    <p>Watchlist<p>
+                                </div>
+                            </div>
+                            <div class="movie-description">
+                                <p>${fullData.Plot}</p>
+                            </div>
+                        </div>
+                    </div>
+                    `;
+            }
+        }
     }
+    if(!foundMovie) moviesHtml = `<p class="result-not-found">Unable to find what you’re looking for. Please try another search.</p>`
     searchResults.innerHTML = moviesHtml;
 }
+
+document.addEventListener("click", function(e){
+    if (e.target.classList.contains("watchlist-btn")){
+        const movieID = e.target.dataset.movieId;
+        if(!watchList.includes(movieID))
+            watchList.push(movieID);
+        localStorage.setItem("watchlist", JSON.stringify(watchList));
+    }
+})
---
+++ watchlist.js
@@ -0,0 +1,50 @@
+const apikey = import.meta.env.VITE_API_KEY;
+const watchListContainer = document.getElementById("watchlist-arr")
+const watchList = JSON.parse(localStorage.getItem("watchlist"))
+
+async function renderPage(){
+    let watchListHtml = "";
+    for (let movie of watchList){
+        const res = await fetch(`https://www.omdbapi.com/?apikey=${apikey}&i=${movie}`);
+        const data = await res.json();
+        watchListHtml += `
+        <div class="movies">
+            <img class="movie-poster" src="${data.Poster}" alt="${data.Title} Poster">
+            <div class="movie-wrapper">
+                <div class="movie-title-rating">
+                    <h3 class="movie-title">${data.Title}</h3>
+                    <div class="rating">
+                        <img src="/images/star.png" alt="Yellow Star">
+                        <p>${data.imdbRating}</p>
+                    </div>
+                </div>
+                <div class="movie-runtime-genre">
+                    <p>${data.Runtime}</p>
+                    <p>${data.Genre}</p>
+                    <div class="btn-wrapper">
+                        <button class="remove-btn" data-movie-id="${movie}">-</button>
+                        <p>Remove<p>
+                    </div>
+                </div>
+                <div class="movie-description">
+                    <p>${data.Plot}</p>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+    watchListContainer.innerHTML = watchListHtml;
+}
+
+renderPage();
+
+               
+document.addEventListener("click", function(e){
+    if (e.target.classList.contains("remove-btn")){
+        const movieID = e.target.dataset.movieId;
+        const index = watchList.indexOf(movieID);
+        watchList.splice(index, 1);
+        localStorage.setItem("watchlist", JSON.stringify(watchList));
+    }
+    renderPage();
+})
